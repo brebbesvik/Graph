e563ad2f19fcce9e16bad56320c2b50d015dbe9d
brebbesvik <brebbesvik@gmail.com>
2018-11-19 13:14:49 +0100

    Added unit test for the workflow model

---
+++ __tests__/WorkflowGraph.test.js
@@ -0,0 +1,28 @@
+const json = require('./asthma-test');
+const WorkflowGraph = require('../src/WorkflowGraph');
+
+const graph = new WorkflowGraph();
+
+beforeAll(() => {
+    graph.buildGraph(json["workflow-model"]);
+});
+
+test('the number of vertices should be 3', ()=>{
+    expect(graph.numVertices()).toBe(3);
+});
+
+test('the number of edges should be 2', ()=>{
+    expect(graph.numEdges()).toBe(2);
+});
+
+test('searching for a Init vertex should return a Init vertex', ()=> {
+    expect(graph.getVertex("Init").getObject()).toMatch("Init");
+});
+
+test('vertex treat should have 1 incoming edge', ()=> {
+    expect(graph.getVertex("Treat").getIncomingEdges()).toBe(1);
+});
+
+test('vertex init should have 2 outgoing edges', ()=> {
+    expect(graph.getVertex("Init").getOutgoingEdges()).toBe(2);
+});
---
+++ __tests__/WorkflowInstanceGraph.test.js
@@ -0,0 +1,28 @@
+const json = require('./asthma-test');
+const WorkflowInstanceGraph = require('../src/WorkflowInstanceGraph');
+
+const graph = new WorkflowInstanceGraph();
+
+beforeAll(() => {
+    graph.buildGraph(json["workflow-instance2"])
+});
+
+test('the number of vertices should be 2', ()=>{
+    expect(graph.numVertices()).toBe(2);
+});
+
+test('the number of edges should be 1', ()=>{
+    expect(graph.numEdges()).toBe(1);
+});
+
+test('searching for a vertex id 1 should return a vertex of id 1', ()=> {
+    expect(graph.getVertexFromId("1").getObject().id).toMatch("1");
+});
+
+test('searching for a vertex id 2 should return a vertex with name Treatment', ()=> {
+    expect(graph.getVertexFromId("2").getObject().name).toMatch("Treatment");
+});
+
+test('searching for a vertex with id 1 should return a vertex with type Init', ()=> {
+    expect(graph.getVertexFromId("1").getObject().type).toMatch("Init");
+});
